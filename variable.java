61a323a91e19ba737fdec05dc8af4cbf96cedbbd
Your Name <you@example.com>
2025-08-10 20:02:57 +0530

    first variable

---
+++ variable.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class variable {
+    public static void main(String[] args) {
+        // Declaring and initializing variables
+        int a = 5;
+        int b = 10;
+        int s = a + b;
+        String n="akshay";
+        double q=18.22;
+        // Printing the values of the variables
+        System.out.println("Value of a: " + a);
+        System.out.println("Value of b: " + b);
+        System.out.println("Sum of a and b: " + s);
+        System.out.println("name:" + n);
+        System.out.println("double value: " + q);
+    }
+    
+}
